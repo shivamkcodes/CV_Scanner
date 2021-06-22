90e177a3624af7772eb30b76407da03599160d84
shivam <shivamguglani306@gmail.com>
2021-06-22 06:50:37 +0530

    deploying changes

--- index2.js
+++ index2.js
@@ -1,100 +1,73 @@
-console.log('Welcome to the fetch api');
-
+console.log("Welcome to the fetch api");
 
 function getData() {
-    URL = 'https://randomuser.me/api?results=22';
-    fetch(URL).then((Response) => {
-        return Response.json();
-
-    }).then((data) => {
-        console.log(data);
-
+  URL = "https://randomuser.me/api?results=22";
+  fetch(URL)
+    .then((Response) => {
+      return Response.json();
     })
+    .then((data) => {
+      console.log(data);
+    });
 }
-// let request = new XMLHttpRequest();
-// request.open('get', 'https://randomuser.me/api?results=22', true);
-// request.onload = function () {
-//     console.log('running');
-//     let text = JSON.parse(request.responseText);
-//     console.log(text.results[0]);
-//     console.log(text.results[0].dob.age);
-//     console.log(text.results[0].name);
-// }
-// request.send();
-// let btn = document.getElementById('btn');
-// btn.addEventListener('click', () => {
-
-//     let result = getData();
-//     // let result1 = JSON.parse(result)
-//     // result = getData().info;
-//     console.log(result);
-//     // console.log(result1);
-
-
-
-// })
 
 //by await
-console.log('welcome to async await');
-
-
+console.log("welcome to async await");
 
 async function fun1() {
-    const response = await fetch('https://randomuser.me/api?results=12');
-    const users = await response.json();
-    return users;
+  const response = await fetch("https://randomuser.me/api?results=12");
+  const users = await response.json();
+  return users;
 }
 
 let a = fun1();
 // console.log(a);
 
 a.then((data) => {
-    poeple = data.results
-    // console.log(poeple)
+  poeple = data.results;
+  // console.log(poeple)
 
-    // console.log(phone)
+  // console.log(phone)
 
+  // function cvIterator(poeple) {
+  //     let nextIndex = 0;
+  //     return {
+  //         next: function () {
+  //             return nextIndex < poeple.length ? {
+  //                 value: poeple[nextIndex++],
+  //                 done: false
+  //             } : {
+  //                 done: true
+  //             }
+  //         }
+  //     }
+  // }
 
-    // function cvIterator(poeple) {
-    //     let nextIndex = 0;
-    //     return {
-    //         next: function () {
-    //             return nextIndex < poeple.length ? {
-    //                 value: poeple[nextIndex++],
-    //                 done: false
-    //             } : {
-    //                 done: true
-    //             }
-    //         }
-    //     }
-    // }
+  // const candidates = cvIterator(poeple);
+  nextCv();
 
-    // const candidates = cvIterator(poeple);
-    nextCv();
+  let btn = document.getElementById("btn");
+  btn.addEventListener("click", nextCv);
 
+  function nextCv() {
+    console.log("guglani");
 
-    let btn = document.getElementById('btn');
-    btn.addEventListener('click', nextCv);
+    // const currentCandidate = candidates.next().value
+    for (let i of poeple) {
+      // console.log(i);
+      let name = i.name.first;
+      let age = i.dob.age;
+      let country = i.location.country;
+      let email = i.email;
+      let phone = i.cell;
+      if (true) {
+        let image = document.getElementById("image");
+        let profile = document.getElementById("profile");
+        image.innerHTML = `<img src='${i.picture.large}'> `;
 
-    function nextCv() {
-        console.log('guglani');
+        // console.log(currentCandidate.img);
 
-        // const currentCandidate = candidates.next().value
-        for (let i of poeple) {
-            // console.log(i);
-            let name = i.name.first;
-            let age = i.dob.age;
-            let country = i.location.country;
-            let email = i.email;
-            let phone = i.cell;
-            if (true) {
-                let image = document.getElementById('image')
-                let profile = document.getElementById('profile')
-                image.innerHTML = `<img src='${i.picture.large}'> `;
-
-                // console.log(currentCandidate.img);
-
-                profile.innerHTML = `
+        profile.innerHTML = `
                     <ul class="list-group">
                     <li class="list-group-item">Name :${name}</li>
                     <li class="list-group-item">${age} years old</li>
@@ -103,9 +76,9 @@ a.then((data) => {
                     <li class="list-group-item"> with ${phone} framework</li>
                     
                 </ul> 
-    `
-            }
-        };
-        profile.innerHTML = '';
+    `;
+      }
     }
+    profile.innerHTML = "";
+  }
 });
